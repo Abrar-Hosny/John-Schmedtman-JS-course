75123b85a0062c9d39ff989ebd9f21daaa496e59
Abrar-Hosny <abrarhosny19@gmail.com>
2023-10-30 00:40:10 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,21 @@
+//javascript => is a high-level object-oriented , multi-paradifm
+//programming language
+
+// values and variables
+// 'jonas' => value 
+// 23 => value
+//value s the small unit that we have in js
+//store values in variables
+
+
+console.log("jonas");
+
+//declare a variable
+
+let x ="Abrar";
+
+
+// let variablename [label ] => box = we put in it a value ;
+
+
+console.log(x);
