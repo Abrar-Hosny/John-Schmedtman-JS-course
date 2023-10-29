a9f5f90937c4100ee9b8826ccc8f0392c4dbbc19
Abrar-Hosny <abrarhosny19@gmail.com>
2023-10-30 00:58:52 +0200

    Assiment1-done

---
+++ JavaScript Fundamentals – Part 1-Assiments/Values_and_Variables.js
@@ -0,0 +1,15 @@
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+
+let country = "Egypt";
+let continent="Africa";
+let population="20 millions";
+
+
+
+// 2. Log their values to the console
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- js.js
+++ js.js
@@ -16,6 +16,33 @@ let x ="Abrar";
 
 
 // let variablename [label ] => box = we put in it a value ;
-
+//box which we can store a value
 
 console.log(x);
+console.log(x);
+console.log(x);
+console.log(x);
+
+// one of the importance of the variables that 
+//we can change value easily without needing for
+//to change it manually 
+
+//conventions of naming  variable : 
+//Camal case => first letter is small second word is capital 
+//firstNamePerson ,  hello_my_friend
+// can not start with numbers we will have Syntax error => for error in wirting
+// under score _ , $ , numbers , letters 
+// not  start with numbers , symbols , reserved keywords like new , function 
+// Capital => Person only in oop 
+
+
+// all capitalized for constant that will not change
+let PI = 3.1415 ;
+
+
+
+//Clean code : 
+// Make sure that the variable names are descriptive 
+// بتعبر عن دورها 
+
+let myFirstJob="Programmer" ;
