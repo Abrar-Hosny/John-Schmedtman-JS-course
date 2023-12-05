3bda758939b89727825bd420dba6ac9bee925668
Abrar-Hosny <abrarhosny19@gmail.com>
2023-12-05 05:16:15 +0200

    some changes on challenge#2

--- JS- part2/arraychallenge.js
+++ JS- part2/arraychallenge.js
@@ -2,22 +2,19 @@
 
 const calcTip =function(bill){
     let tip ; 
-    if(bill>=50 && bill<=300){
-        tip = 15/100 *bill ; 
+    return   bill>=50 && bill<=300  ? tip = 15/100 *bill : tip = 20/100 *bill ; 
         
-    }
-    else{
-                tip = 20/100 *bill ; 
-
-        
-    }
-    return tip ; 
 }
+ 
+               
 
 const x= calcTip(100);
 const bills = [125,555,44];
 
 const tips = [calcTip(125), calcTip(555) ,calcTip(44)];
+
+console.log("tips => " , tips); 
+console.log("bills => " , bills); 
 
 let total =[];
 
@@ -25,8 +22,8 @@ for(let x = 0 ; x< 3 ; x++){
     total[x]=bills[x]+tips[x];
 }
 
-for(let y = 0 ; y< 3 ; y++){
-console.log("total 🐝" ,y+1 ,"=> ",total[y]);
+
+console.log("total 🐝" ,total);
     
-}
+
 
